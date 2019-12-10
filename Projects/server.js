07f723a70cd73b8eb04e35aaca095f9c986ab489
Bryan Vargas <BVargas949@gmail.com>
2019-12-10 16:46:17 -0700

    Reviewing Star Wars Api

--- Projects/server.js
+++ Projects/server.js
@@ -20,52 +20,35 @@ app.get('/', (req, res) => {
       (json) => {
         console.log(json)
       res.render('index', { 
-          data: planet,
           planet: json
         })
       })
     });
 
-    var planet = {
-      "name": "Naboo",
-      "rotation_period": "26",
-      "orbital_period": "312",
-      "diameter": "12120",
-      "climate": "temperate",
-      "gravity": "1 standard",
-      "terrain": "grassy hills, swamps, forests, mountains",
-      "surface_water": "12",
-      "population": "4500000000",
-      "residents": [
-        "https://swapi.co/api/people/3/",
-        "https://swapi.co/api/people/21/",
-        "https://swapi.co/api/people/36/",
-        "https://swapi.co/api/people/37/",
-        "https://swapi.co/api/people/38/",
-        "https://swapi.co/api/people/39/",
-        "https://swapi.co/api/people/42/",
-        "https://swapi.co/api/people/60/",
-        "https://swapi.co/api/people/61/",
-        "https://swapi.co/api/people/66/",
-        "https://swapi.co/api/people/35/"
-      ],
-      "films": [
-        "https://swapi.co/api/films/5/",
-        "https://swapi.co/api/films/4/",
-        "https://swapi.co/api/films/6/",
-        "https://swapi.co/api/films/3/"
-      ],
-      "created": "2014-12-10T11:52:31.066000Z",
-      "edited": "2014-12-20T20:58:18.430000Z",
-      "url": "https://swapi.co/api/planets/8/"
-    }
-
-    app.get('/page0', (req, res) => {
-        res.render('page0', 
-        { 
-          data: info
-        });
-      });
+    // var planet = {
+    //   "name": "Yavin IV",
+    //   "rotation_period": "24",
+    //   "orbital_period": "4818",
+    //   "diameter": "10200",
+    //   "climate": "temperate, tropical",
+    //   "gravity": "1 standard",
+    //   "terrain": "jungle, rainforests",
+    //   "surface_water": "8",
+    //   "population": "1000",
+    //   "residents": [],
+    //   "films": [
+    //     "https://swapi.co/api/films/1/"
+    //   ],
+    //   "created": "2014-12-10T11:37:19.144000Z",
+    //   "edited": "2014-12-20T20:58:18.421000Z",
+    //   "url": "https://swapi.co/api/planets/3/"
+    // }
+    // app.get('/page0', (req, res) => {
+    //     res.render('page0', 
+    //     { 
+    //       data: info
+    //     });
+    //   });
 
 
     const server = app.listen(port, () =>{
